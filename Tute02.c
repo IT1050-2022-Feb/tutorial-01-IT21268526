eaa47bb803d1a7c5149fdd17a67da0f73e349f88
jayarathna <99312661+IT21268526@users.noreply.github.com>
2022-02-09 06:18:49 +0000

    completed

--- Tute02.c
+++ Tute02.c
@@ -17,8 +17,35 @@ Distance -> 50
 Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 #include <stdio.h>
+//diclare varibles
+int main() 
+{
+ float total amount;
+ int r_dist=0,dist=0;
+//get inputs
+ print("Enter the distance:");
+ scanf("%f",&dist);
 
-int main() {
-  
-  return 0;
+ if(dist>=30)
+ {
+   r-dist=dist-30;
+   tempamount=r_dist*40;
+   total amount=30*50+tempamount;
+
+   printf("Amount is:%2f",total amount);
+   
+ }
+ else
+ {
+   total amount=dist*50.00;
+   printf("Amount is:%2f",total amount);
+ }
+//end of the diclaration
+ return 0;
 }
+
+
+
+
+  
+
