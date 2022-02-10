820dd4382f5f18d33782e205c2d5d34e959aba59
jayarathna <99312661+IT21268526@users.noreply.github.com>
2022-02-10 10:15:14 +0000

    completed

--- Tute04.c
+++ Tute04.c
@@ -6,14 +6,53 @@ Do not change the code given in the main() function when you are implementing yo
 
 #include <stdio.h>
 
-int main() {
-   int no1, no2;
-   printf("Enter a value for no 1 : ");
+#include <stdio.h>
+
+int minimum(int no1, int no2); //Functions Decleration
+int maximum(int no1, int no2);
+int multiply(int no1, int no2);
+
+int main() //Main Function
+{
+   int no1, no2; //Declaring Variables
+  
+   printf("Enter a value for no 1 : "); // Taking Inputs
    scanf("%d", &no1);
    printf("Enter a value for no 2 : ");
    scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
+   
+   printf("%d ", minimum(no1, no2)); //Functions invoking and taking outputs
    printf("%d ", maximum(no1, no2));
    printf("%d ", multiply(no1, no2));
+  
    return 0;
+}
+
+int minimum(int no1, int no2) //Functions Implimentation
+{
+  if (no1<no2)
+  {
+    return no1;
+  }
+  else
+  {
+    return no2;
+  }
+}
+
+int maximum(int no1, int no2)
+{
+   if (no1>no2)
+  {
+    return no1;
+  }
+  else
+  {
+    return no2;
+  }
+}
+
+int multiply (int no1, int no2)
+{
+  return no1 * no2;
 }
